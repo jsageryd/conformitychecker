4f037d98e5ae85cce167a5fc8797359ceb4d8120
Johan Sageryd <j416@1616.se>
2010-05-27 08:52:44 +0200

    Initial commit

---
+++ conformitychecker.php
@@ -0,0 +1,17 @@
+function conformitychecker($string){
+#	hira 3040-309F   kata 30A0-30FF   kanji 4E00-9FC2
+	$count = preg_match("/\x{4E00}-\x{9FC2}/", $string);
+
+	return $string . $count;
+}
+
+$e = &$modx->Event;
+switch ($e->name) {
+	case "OnLoadWebDocument":
+		$o = &$modx->documentObject['content']; // get a reference of the output
+		$o = conformitychecker($o);
+		break;
+	default :
+		return; // stop here - this is very important.
+		break;
+}
