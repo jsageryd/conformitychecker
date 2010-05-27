52714f8921cbf96bd6deee39d857816d97107672
Johan Sageryd <j416@1616.se>
2010-05-27 10:33:03 +0200

    Hide counter

--- conformitychecker.php
+++ conformitychecker.php
@@ -2,7 +2,7 @@ function conformitychecker($string){
 #	hira 3040-309F   kata 30A0-30FF   kanji 4E00-9FC2
 	$count = preg_match("/\x{4E00}-\x{9FC2}/", $string);
 
-	return $string . $count;
+	return "<div style=\"display: none;\">" . $count . "</div>" . $string;
 }
 
 $e = &$modx->Event;
