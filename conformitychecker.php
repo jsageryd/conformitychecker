6c3d1a2c17f3d280d67cdacd116434e8cd0aa2ea
Johan Sageryd <j416@1616.se>
2010-05-27 13:12:48 +0200

    Add functionality

--- conformitychecker.php
+++ conformitychecker.php
@@ -1,8 +1,34 @@
 function conformitychecker($string){
-#	hira 3040-309F   kata 30A0-30FF   kanji 4E00-9FC2
-	$count = preg_match("/\x{4E00}-\x{9FC2}/", $string);
+	$s = findkanjiwithouttooltip($string);
+	return $string if(empty($s));
+	return '<div class="note">' . $s . '</div>' . $string;
+}
 
-	return "<div style=\"display: none;\">" . $count . "</div>" . $string;
+function findkanjiwithouttooltip($string){
+//	Ranges: hira 3040-309F   kata 30A0-30FF   kanji 4E00-9FC2
+
+//	Total kanji or kanji compounds
+	preg_match_all("/([\x{4E00}-\x{9FC2}]+)/u", $string, $total);
+
+//	Kanji or kanji compounds inside a tooltip tag
+	preg_match_all("/\\\\tt\[.*([\x{4E00}-\x{9FC2}]+).*\]/uU", $string, $withtooltip);
+
+//	Add all kanji to hash
+	foreach($total[1] as $k){ $withouttooltip[$k] = 1; }
+
+//	Remove those with tooltips
+	foreach($withtooltip[1] as $k){ unset($withouttooltip[$k]); }
+
+	$count = sizeof($withouttooltip);
+
+//	Return if there are none without tooltips
+	if($count == 0) return "";
+
+//	Create info string
+	$string = "There are " . $count . " kanji or kanji compounds without a tooltip: ";
+	$string .= join(array_keys($withouttooltip), ', ');
+
+	return $string;
 }
 
 $e = &$modx->Event;
